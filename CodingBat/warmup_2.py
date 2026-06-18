cf1b10c5805cb9b881d60e625218baf637e63438
Devanshu-panda <devanshupanda.dev@gmail.com>
2026-06-19 00:36:26 +0530

    Add solutions to warmup_2
    Implement string_times function to repeat a string n times.

--- CodingBat/warmup_2.py
+++ CodingBat/warmup_2.py
@@ -1,1 +1,11 @@
+#Problem_1: string_times
+def string_times(str, n):
+    word = str
+    if n>0:
+        for x in range(n-1):
+            word += str
+        return word
+    elif n==0:
+        return ""
+
 
