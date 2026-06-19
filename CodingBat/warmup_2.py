564355952903fa4a55d03d2580dad8c9481cc7bf
Devanshu-panda <devanshupanda.dev@gmail.com>
2026-06-19 23:41:48 +0530

    Add solutions to warmup_2.py

--- CodingBat/warmup_2.py
+++ CodingBat/warmup_2.py
@@ -38,4 +38,73 @@ def string_splosion(str):
     word += str[:i]
   return word
 
+#Problem_5: last2
+def last2(str):
+  n = len(str)
+  sub_str = str[n-2:]
+  count = 0
+  if n <= 1:
+    return 0
+  else:
+    for i in range(n):
+        if i+2 <= n:
+            if sub_str in str[i : i+2]:
+                count += 1
+    return count - 1
+
+#Problem_6: array_count9
+def array_count9(nums):
+  n = len(nums)
+  count = 0
+  for i in range(n):
+    if nums[i] == 9:
+      count += 1
+  return count
+
+#Problem_7: array_front9
+def array_front9(nums):
+  if len(nums) <= 4:
+    if 9 in nums:
+      return True
+    else:
+      return False
+  else:
+    if 9 in nums[:4]:
+      return True
+    else:
+      return False
+
+#Problem_8: array123
+def array123(nums):
+    n = len(nums)
+    all_sub_array = []
+    for i in range(n):
+        if i + 3 <= n:
+            all_sub_array += [nums[i:i + 3]]
+    if [1, 2, 3] in all_sub_array:
+        return True
+    else:
+        return False
+
+#Problem_9: string_match
+def string_match(a, b):
+    len_a = len(a)
+    len_b = len(b)
+    set_a = []
+    set_b = []
+    count = 0
+    for i in range(len_a-1):
+        set_a += [a[i:i+2]]
+    for i in range(len_b-1):
+        set_b += [b[i:i+2]]
+    if len_a >= len_b:
+        for i in range(len(set_b)):
+            if set_b[i] == set_a[i]:
+                count += 1
+    else:
+        for i in range(len(set_a)):
+            if set_b[i] == set_a[i]:
+                count += 1
+    return count
+
 
