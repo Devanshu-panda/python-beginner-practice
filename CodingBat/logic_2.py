52615741ea0676632a3cfeb6380583d901791227
Devanshu-panda <devanshupanda.dev@gmail.com>
2026-06-21 15:21:33 +0530

    Add solutions logic_2.py

--- CodingBat/logic_2.py
+++ CodingBat/logic_2.py
@@ -1,1 +1,53 @@
+#Problem_1: make_bricks
+def make_bricks(small, big, goal):
+  rem_1 = goal % 5
+  q = goal // 5
+  if rem_1 == 0:
+    if goal/5 <= big:
+      return True
+    elif goal/5 >= big:
+      return(goal - big*5 <= small)
+  else:
+    if q >= big:
+      return(goal - big*5 <= small)
+    elif q<= big:
+      return(goal - q*5 <= small)
 
+#Problem_2: lone_sum
+def lone_sum(a, b, c):
+  sum = 0
+  if a != b and a != c: 
+   sum += a
+  if b != a and b != c:
+    sum += b
+  if c != a and c != b:
+    sum += c
+  return sum
+
+#Problem_3: lucky_sum
+def lucky_sum(a, b, c):
+  sum = 0
+  if a != 13:
+    sum += a
+    if b != 13:
+      sum += b
+      if c != 13:
+        sum += c
+      else:
+        sum = sum
+    else:
+      sum = sum
+  else:
+    sum = sum
+  return sum
+
+#Problem_4: no_teen_sum
+def no_teen_sum(a, b, c):
+ return fix_teen(a) + fix_teen(b) + fix_teen(c)
+ 
+def fix_teen(n):
+  if n >= 13 and n < 15 or n > 16 and n <= 19: 
+    n = 0
+  return n
+
+#Problem_5: 
