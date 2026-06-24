651b3a9289bcfee39ae80c731bd27313e274a60a
Devanshu-panda <devanshupanda.dev@gmail.com>
2026-06-24 22:19:02 +0530

    Add solutions to list_2.py

--- CodingBat/list_2.py
+++ CodingBat/list_2.py
@@ -1,1 +1,18 @@
+#Problem_1: count_evens
+def count_evens(nums):
+  count = 0
+  for i in range(len(nums)):
+    if nums[i] % 2 == 0:
+      count += 1
+  return count
 
+#Problem_2: big_diff
+def big_diff(nums):
+  largest = nums[0]
+  smallest = nums[0]
+  for i in range(len(nums)):
+    if largest < nums[i]:
+      largest = nums[i]
+    elif smallest > nums[i]:
+      smallest = nums[i]
+  return largest-smallest
