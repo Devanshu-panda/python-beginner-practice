02591709188c36d554add6fbece83eabb3b08969
Devanshu-panda <devanshupanda.dev@gmail.com>
2026-06-25 21:35:16 +0530

    Add solutions to list_2.py

--- CodingBat/list_2.py
+++ CodingBat/list_2.py
@@ -16,3 +16,31 @@ def big_diff(nums):
     elif smallest > nums[i]:
       smallest = nums[i]
   return largest-smallest
+
+#Problem_3: centered_average
+def centered_average(nums):
+  n = len(nums)
+  total = 0
+  smallest = nums[0]
+  largest = nums[0]
+  for i in range(n):
+    if largest < nums[i]:
+      largest = nums[i]
+    if smallest > nums[i]:
+      smallest = nums[i]
+  for i in nums:
+    total += i
+  return  (total - smallest - largest)/(n-2)
+
+#Problem_6: has22
+def has22(nums):
+  sub_array = []
+  n = len(nums)
+  for i in range(n-1):
+    sub_array += [nums[i:i+2]]
+  if [2,2] in sub_array:
+    return True
+  else:
+    return False
+
+
